f9734b8b4ed7e6d41af9497087256e843baee13f
Felipe <felipefreitasdaschagas@hotmail.com>
2022-03-10 06:18:46 -0300

    iniciando arrays com media

---
+++ aula1.js
@@ -0,0 +1,14 @@
+//medias 10, 6.5,8,7.5
+
+//let media1 = 10
+//let media2 = 6.5
+//let media3 = 8
+//let media4 = 7.5
+
+//let mediaFinal = (media1 + media2 + media3 + media4)/4
+
+const notas = [10, 6.5, 8, 7.5]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3])/ notas.length
+
+console.log(media)
