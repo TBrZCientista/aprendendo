08d111ca3dd2ce31338545fa4e49f84389a777f7
Felipe <felipefreitasdaschagas@hotmail.com>
2022-03-11 06:31:41 -0300

    Alterando uma lista de nomes com splice

---
+++ aula5.js
@@ -0,0 +1,7 @@
+// criando lista com nomes que serão modificados com saida de dados e entrada logo em seguida. Uma atualização da lista.
+
+const listaDeChamada =['João','Ana','Caio','Marjorie','Leo']
+
+listaDeChamada.splice(1,2,'Rodrigo')
+
+console.log(`A lista de chamada: ${listaDeChamada}`)
