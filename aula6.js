2aefa6b3ab852db78c4cd0f896d0bc01b5df4c1f
Felipe <felipefreitasdaschagas@hotmail.com>
2022-03-11 12:43:44 -0300

    aprendendo concatenação de arrays com o concat

---
+++ aula6.js
@@ -0,0 +1,8 @@
+// vamos juntar arrays nessa aula (concatenação de arrays)
+
+const salaDePyton = ['Melssa','Helena','Rodrigo']
+const salaDeJavascript = ['Jú','Leo','Raquel']
+
+const salasUnificadas = salaDePyton.concat(salaDeJavascript)
+
+console.log(`Os alunos da sala unificada são: ${salasUnificadas}`)
