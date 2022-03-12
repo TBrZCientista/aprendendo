49993c8c840c0d7881bf127f4b8ae2f6b8ef1cb5
Felipe <felipefreitasdaschagas@hotmail.com>
2022-03-12 06:52:24 -0300

    Calculando média com o forEach

---
+++ aula11.js
@@ -0,0 +1,15 @@
+// caldulando media com forEach
+
+const notas = [10, 6.5, 8 , 7.5]
+
+let somaDasNotas = 0
+
+// o forEach é uma função callback, é uma função que chama de volta outra função. o forEach faz a busca no índice do array sem ter a declaração solicitada na função
+
+notas.forEach(nota =>{
+    somaDasNotas += nota;
+})
+
+let media = somaDasNotas/notas.length
+
+console.log(`A media das notas é ${media}`)
